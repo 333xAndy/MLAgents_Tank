6191ddbc99aeb9d03c6b23ecb92b412ed61378c3
Andy De Leon <66084673+333xAndy@users.noreply.github.com>
2021-06-09 11:59:16 -0700

    Add files via upload
    Script for shell behavior.

---
+++ Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+   private Rigidbody m_Bullet;
+   private float speed = 15f;
+
+    void Start(){
+       m_Bullet = GetComponent<Rigidbody>();
+       Invoke("Destroy", 5f);
+   }
+
+    void Update(){
+       Vector3 move = transform.forward * speed * Time.deltaTime;
+       m_Bullet.MovePosition(m_Bullet.position + move);
+   }
+
+   void Destroy(){
+       Destroy(gameObject);
+   }
+
+   private void OnCollisionEnter(Collision collision){
+       CancelInvoke();
+       Destroy(gameObject, 1f);
+       enabled = false;
+   }
+}
